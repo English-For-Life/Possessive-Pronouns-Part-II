3ea29ab8b6a1a10a7490cf88e097c35e36c6bd35
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:04:36 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Is that your car? - No, ___ is over there.",
+        options:["its", "mine", "my"],
+        answer: 1
+    },
+    {
+     q:"Is that green bike Ann's? - No, ___ is blue.",
+        options:["hers", "she", "her"],
+        answer: 0   
+    },
+    {
+        q:"Please give this book to Alice. It's ___ .",
+        options:["she", "her", "hers"],
+        answer: 2
+    },
+    {
+        q:"Please take this key ring as a gift from me. Here. It's ___ . ",
+        options:[ "you", "your", "yours"],
+        answer: 2
+    },
+    {
+        q:"Is this his watch? - Yes, it's ___ .",
+        options:["his", "he", "him",],
+        answer: 0
+    },
+    {
+        q:"We gave them our telephone number, and they gave us ___ .",
+        options:["their", "theirs", "they"],
+        answer: 1
+    },
+    {
+        q:"Is that our bus? - No, ___ arrives in 20 minutes.",
+        options:["ours", "us", "our"],
+        answer: 0
+    },
+    {
+        q:"Is this my pen? - No, it's not ___ . It's mine.",
+        options:["your", "yours", "you"],
+        answer: 1
+    },
+    {
+        q:"Is that your motorbike in the driveway? - No, we parked ___ in the street.",
+        options:["us", "ours", "our"],
+        answer: 1
+    },
+    {
+        q:"This isn't our son's skateboard. ___ is in the car.",
+        options:["Him", "He", "His"],
+        answer: 2
+    }
+
+]
